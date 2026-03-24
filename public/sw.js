6264c232cf4865d6de174ef1792bc92a30e1d8a3
Eoxik <eoxik@h-5fed69fd73d64e94b265206d43baf4a3.centralus.cloudapp.azure.com>
2026-03-24 15:32:07 +0800

    fix: attain pixel-perfect pwa validation with resized assets and robust service worker

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,43 @@
-self.addEventListener('install', (e) => {
-  console.log('[Service Worker] Install');
+const CACHE_NAME = 'gs-pama-v2';
+const assetsToCache = [
+    '/',
+    '/manifest.json',
+    '/icons/icon-192x192.jpg',
+    '/icons/icon-512x512.jpg',
+    '/icons/screenshot-1.jpg'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[SW] Pre-caching core assets');
+            return cache.addAll(assetsToCache);
+        })
+    );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Pass-through fetch handler
-  e.respondWith(fetch(e.request));
+// Activate Event (Cleanup old caches)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (cache !== CACHE_NAME) {
+                        console.log('[SW] Removing old cache:', cache);
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch Event (Network falling back to cache)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
